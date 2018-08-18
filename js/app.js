f5bacca5e6e5d5b0f194b1f30e5722e7a6d4c5a4
Xavier <parsonsxe@live.com>
2018-08-18 19:31:08 -0400

    added functionality to display cost
    needs to display only one div

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const hiddenRole = document.querySelector('#other');
 const designMenu = document.querySelector('#design');
 const colorOptions = document.querySelector('#color');
 const colorOptionsDiv = document.querySelector('#colors-js-puns');
-const registerActivities = document.getElementsByClassName('.activites');
+const registerActivities = document.querySelector('.activities');
 
 
 // Puts focus on Name Field by default
@@ -74,17 +74,53 @@ registerActivities.addEventListener('change', () => {
     let nodejs = document.getElementsByTagName('input')[name="node"];
     let buildTools = document.getElementsByTagName('input')[name="build-tools"];
     let npm = document.getElementsByTagName('input')[name="npm"];
+    // add cost of activities
+    let cost = 0;
+    let displayCostDiv = document.createElement('div');
     
-    // Check for timing conflicts based on checkboxes selected
+    // Check for timing conflicts JS Frameworks & Express
         if (jsFrameworks.checked === true) {
+            cost += 100;
             expressWorkshop.setAttribute('disabled', true);
+            
+        } else {
+            expressWorkshop.removeAttribute('disabled');   
+        }
+        
+        if (expressWorkshop.checked === true) {
+            cost += 100;
+            jsFrameworks.setAttribute('disabled', true);
+            
+        } else {
+            jsFrameworks.removeAttribute('disabled', true);
         }
     
+    // Check for timing conflicts with JS Lib & Node.js
+        if (jsLibs.checked === true) {
+            cost += 100;
+            nodejs.setAttribute('disabled', true);
+            
+        } else {
+            nodejs.removeAttribute('disabled');   
+        }
+        
+        if (nodejs.checked === true) {
+            cost += 100;
+            jsLibs.setAttribute('disabled', true);
+            
+        } else {
+            jsLibs.removeAttribute('disabled', true);
+        }
+    
+    
+        
+        // Display cost of selected activities
+        displayCostDiv.innerHTML = `Your total is ${cost}`;
+        registerActivities.appendChild(displayCostDiv);
+           
+
 });
 
-
-//if JS Frame checked disable Express Workshop
-// if JS Lib checked disable node.js
 
 
 
