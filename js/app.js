21bf6cc0474c218ee8373e8883f1672e6de50963
Xavier <parsonsxe@live.com>
2018-08-19 17:52:43 -0400

    updated payment section

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@
   Treehouse Project 3
   FSJS - Interactive Form 
 ****************************/
-'use strict';
+
+// Variables 
 const nameInput = document.querySelector('#name');
 const titleMenu = document.querySelector('#title');
 const hiddenRole = document.querySelector('#other');
@@ -10,16 +11,38 @@ const designMenu = document.querySelector('#design');
 const colorOptions = document.querySelector('#color');
 const colorOptionsDiv = document.querySelector('#colors-js-puns');
 const registerActivities = document.querySelector('.activities');
+const paymentMethod = document.querySelector('#payment');
+const creditCardDiv = document.querySelector('#credit-card');
+const payPalText = creditCardDiv.nextElementSibling;
+const bitcoinText = payPalText.nextElementSibling;
+
+// Checkbox variables 
+let mainConf = document.getElementsByTagName('input')[name="all"];
+let jsFrameworks = document.getElementsByTagName('input')[name="js-frameworks"]; 
+let jsLibs = document.getElementsByTagName('input')[name="js-libs"];
+let expressWorkshop = document.getElementsByTagName('input')[name="express"];
+let nodejs = document.getElementsByTagName('input')[name="node"];
+let buildTools = document.getElementsByTagName('input')[name="build-tools"];
+let npm = document.getElementsByTagName('input')[name="npm"];
+    
 
 
 // Puts focus on Name Field by default
 nameInput.focus();
 
-// Hides 'Other' role input
+// Hide 'Other' role input
 hiddenRole.style.display = 'none';
 
-// Hides color options by default
+// Hide color options 
 colorOptionsDiv.style.display = 'none';
+
+// Hide payment options
+creditCardDiv.style.display = 'none';
+
+// Hide Paypal & Bitcoin text
+payPalText.style.display = 'none';
+bitcoinText.style.display = 'none';
+
 
 
 // On menu change show hiddenRole option
@@ -30,6 +53,7 @@ titleMenu.addEventListener('change', () => {
       hiddenRole.style.display = 'none';
   }     
 });
+
 
 
 // Show correct options based on theme choice
@@ -65,22 +89,11 @@ designMenu.addEventListener('change', () => {
 });
 
 
+// Checks for conflicts with registered activities
 registerActivities.addEventListener('change', () => {
-    // Variables to manipulate
-    let mainConf = document.getElementsByTagName('input')[name="all"];
-    let jsFrameworks = document.getElementsByTagName('input')[name="js-frameworks"]; 
-    let jsLibs = document.getElementsByTagName('input')[name="js-libs"];
-    let expressWorkshop = document.getElementsByTagName('input')[name="express"];
-    let nodejs = document.getElementsByTagName('input')[name="node"];
-    let buildTools = document.getElementsByTagName('input')[name="build-tools"];
-    let npm = document.getElementsByTagName('input')[name="npm"];
-    // add cost of activities
-    let cost = 0;
-    let displayCostDiv = document.createElement('div');
     
     // Check for timing conflicts JS Frameworks & Express
         if (jsFrameworks.checked === true) {
-            cost += 100;
             expressWorkshop.setAttribute('disabled', true);
             
         } else {
@@ -88,7 +101,6 @@ registerActivities.addEventListener('change', () => {
         }
         
         if (expressWorkshop.checked === true) {
-            cost += 100;
             jsFrameworks.setAttribute('disabled', true);
             
         } else {
@@ -97,7 +109,6 @@ registerActivities.addEventListener('change', () => {
     
     // Check for timing conflicts with JS Lib & Node.js
         if (jsLibs.checked === true) {
-            cost += 100;
             nodejs.setAttribute('disabled', true);
             
         } else {
@@ -105,35 +116,48 @@ registerActivities.addEventListener('change', () => {
         }
         
         if (nodejs.checked === true) {
-            cost += 100;
             jsLibs.setAttribute('disabled', true);
             
         } else {
             jsLibs.removeAttribute('disabled', true);
         }
     
-    
         
-        // Display cost of selected activities
-        displayCostDiv.innerHTML = `Your total is ${cost}`;
-        registerActivities.appendChild(displayCostDiv);
-           
+        // let cost = 0;
+        // let displayCostDiv = document.createElement('div');
+        // registerActivities.appendChild(displayCostDiv);
 
 });
 
 
 
 
+paymentMethod.addEventListener('change', () => {
+    let paymentOption = paymentMethod.options[paymentMethod.selectedIndex].value;
+    
+    if (paymentOption === 'select_method') {
+        creditCardDiv.style.display = 'none';
+        payPalText.style.display = 'none';
+        bitcoinText.style.display = 'none';
+    }
+    
+    if (paymentOption === 'credit card') {
+        creditCardDiv.style.display = 'block';
+    }
+    
+    if (paymentOption === 'paypal') {
+        creditCardDiv.style.display = 'none';
+        bitcoinText.style.display = 'none';
+        payPalText.style.display = 'block';     
+    } 
+    
+    if (paymentOption === 'bitcoin') {
+        bitcoinText.style.display = 'block';
+        payPalText.style.display = 'none';
+        creditCardDiv.style.display = 'none';
+    }
+    
+});
 
 
 
-
-
-
-
-
-
-
-
-
-// Payment Method
